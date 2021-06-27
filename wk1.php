b7ebbdc012e3b609f8d50b28fa539439e0dd638a
Duenize <duedav@gmail.com>
2021-06-27 23:40:39 +0100

    First commit

---
+++ wk1.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$a = 10;
+$b = 20;
+$sum = $a +$b;
+echo $sum;
+
+
+?><br><br>
+
+
+<?php 
+//range
+$num =range(0,10); // foreach makes the $num to be echoed as $r
+foreach ($num as $r){
+	echo "$r";
+}
+
+?>
